becccb4f63d2ebc90ddd1f71ceffc3e17e30d278
Brennon Nuckols <brennuck@gmail.com>
2020-03-25 13:34:35 -0600

    day 2 mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,6 +5,7 @@ import sys
 LDI = 0b10000010
 PRN = 0b01000111
 HLT = 0b00000001
+MUL = 0b10100010
 
 class CPU:
     """Main CPU class."""
@@ -15,26 +16,29 @@ class CPU:
         self.reg = [0] * 8
         self.ram = [0] * 256
 
-    def load(self):
+    def load(self, filename):
         """Load a program into memory."""
+        try:
+            address = 0
+            # Open the file
+            with open(sys.argv[1]) as f:
+                # Read all the lines
+                for line in f:
+                    # Parse out comments
+                    comment_split = line.strip().split("#")
+                    # Cast number strings to ints
+                    value = comment_split[0].strip()
+                    # Ignore blank lines
+                    if value == "":
+                        continue
+                    instruction = int(value, 2)
+                    # Populate a memory array
+                    self.ram[address] = instruction
+                    address += 1
+        except FileNotFoundError:
+            print("File not found")
+            sys.exit(2)
 
-        address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
 
     def ram_read(self, mar):
         mdr = self.ram[mar]
@@ -49,6 +53,8 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+        elif op == MUL:
+            self.reg[reg_a] *= self.reg[reg_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -85,6 +91,9 @@ class CPU:
             elif opcode == PRN:
                 print(self.reg[operand_a])
                 self.pc += 2
+            elif opcode == MUL:
+                self.alu(opcode, operand_a, operand_b)
+                self.pc += 3
             elif opcode == HLT:
                 sys.exit(0)
             else:
